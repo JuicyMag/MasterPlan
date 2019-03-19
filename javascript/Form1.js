09e911c6ea95cbc3d4507c273c6d8f6d6865c180
Amelia <asc4@williams.edu>
2019-03-19 08:26:08 +0800

    turns out I have no idea how to append to a csv file using js

---
+++ javascript/Form1.js
@@ -0,0 +1,15 @@
+
+
+$(document).on('click', '#Submit', function(){
+
+	var student = document.getElementById('Name').value + 
+					',' + document.getElementById('ClassYear').value + '\n';
+	console.log(student);
+
+
+	var fso = new ActiveXObject("Scripting.FileSystemObject"); 
+	var s = fso.OpenTextFile("../Students.csv", 8); 
+	s.WriteLine("helloworld"); 
+	s.Close();
+
+});
